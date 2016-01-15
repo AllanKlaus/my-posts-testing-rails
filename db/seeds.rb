9e1e7a8f8bb5a837d1d4b1b0c86ecfe020a2224d
Allan Klaus <allan_klaus@globo.com>
2016-01-14 18:21:59 -0800

    First Commit

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+   categories = Category.create([{ category: 'Ruby' }, { category: 'Ruby on Rails' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
